5a2b05fa6838fffd24335440625c6e748aa919af
J.M <123191439+Maria9is@users.noreply.github.com>
2026-04-30 17:57:10 -0500

    Initialize Streamlit app with basic layout

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Aquí empezamos a diseñar la parte visual
+st.title("🥓 Chorizos Sabor & Tradición 🍗")
+st.subheader("¡Bienvenido a nuestro catálogo interactivo!")
+
+st.write("Hola, esta es la primera prueba de mi tienda en internet.")
+
+# Un mensaje de éxito con color
+st.success("¡La aplicación está funcionando perfectamente!")
